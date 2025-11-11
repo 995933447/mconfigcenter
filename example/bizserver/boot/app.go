7fe371f7a4255045fa651611d386f092256caa87
995933447 <34621597+995933447@users.noreply.github.com>
2025-11-11 16:30:20 +0100

    Create app.go

---
+++ example/bizserver/boot/app.go
@@ -0,0 +1,19 @@
+package boot
+
+import (
+	"log"
+
+	"github.com/995933447/mconfigcenter/example/bizserver/config"
+	"github.com/995933447/mconfigcenter/example/bizserver/confighub"
+)
+
+func InitApp() {
+	var listenerGroup string
+	config.SafeReadServerConfig(func(c *config.ServerConfig) {
+		listenerGroup = c.ListenerGroup
+	})
+	err := confighub.LoadConfigHub(listenerGroup)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
