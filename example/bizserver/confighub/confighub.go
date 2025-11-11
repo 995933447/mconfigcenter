adbdca7691b88885f674d92500b3ca1106ed631c
995933447 <34621597+995933447@users.noreply.github.com>
2025-11-11 16:32:34 +0100

    Create confighub.go

---
+++ example/bizserver/confighub/confighub.go
@@ -0,0 +1,18 @@
+package confighub
+
+import (
+	"github.com/995933447/mconfigcenter/configcenter"
+	"github.com/995933447/mconfigcenter/example/biz"
+	"github.com/995933447/mconfigcenter/example/common/confighub/generalkv"
+)
+
+func LoadConfigHub(listenerGroup string) error {
+	if err := configcenter.InitReconfmgrReloader(biz.EasymicroDiscoveryName, listenerGroup); err != nil {
+		return err
+	}
+	if err := generalkv.RegisterKVConfig(); err != nil {
+		return err
+	}
+	generalkv.RegisterVipLevelMaxConfig()
+	return nil
+}
