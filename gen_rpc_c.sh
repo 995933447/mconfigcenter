253e21c39be8424c5038e861f04c77efe980c644
995933447 <34621597+995933447@users.noreply.github.com>
2025-11-12 21:58:48 +0100

    Add files via upload

--- gen_rpc_c.sh
+++ gen_rpc_c.sh
@@ -8,13 +8,11 @@ mkdir -p $1
 protoc --go_out=./$1\
   --go-grpc_out=./$1\
   --easymicro-client_out=./$1\
-  --easymicro-server_out=./$1\
   --mgorm_out=./$1\
   --jsonschema_out=./$1\
   --go_opt=paths=source_relative\
   --go-grpc_opt=paths=source_relative\
   --easymicro-client_opt=paths=source_relative\
-  --easymicro-server_opt=paths=source_relative\
   --mgorm_opt=paths=source_relative\
   --jsonschema_opt=paths=source_relative\
   --proto_path=./proto\
